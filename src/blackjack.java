f6869fcb7a1604a7a9de5c5494bec34a2b0f0f56
mghgroenen <mgh.groenen@gmail.com>
2020-08-28 07:24:07 -0700

    Blackjack tweede commit

--- src/blackjack.java
+++ src/blackjack.java
@@ -1,17 +1,20 @@
+import java.util.Arrays;
+import java.util.Collections;
 
 public class blackjack {
 	public static void main(String[] args) {
 		String kleur [] = {"Harten", "Ruiten", "Schoppen", "Klaveren"};
-		String waarde [] = {"Boer", "Vrouw", "Heer", "Aas"};
-		String kaarten [] = new String[16];
+		String waarde [] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Vrouw", "Heer", "Aas"};
+		String kaarten [] = new String[52];
 		int counter = 0;
 		
 		for(int i=0; i<kleur.length;i++) {
-			for(int j=0; j<waarde.length;i++) {
+			for(int j=0; j<waarde.length;j++) {
 				kaarten[counter] = kleur[i] + " " + waarde[j];
 				counter++;
 			}
 		}
+		Collections.shuffle(Arrays.asList(kaarten));
 		for(String check : kaarten) {
 			System.out.println(check);
 		}

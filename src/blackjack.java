76d094c695fbfdf47343e30f01370c6641fe22f3
mghgroenen <mgh.groenen@gmail.com>
2020-08-31 07:45:52 -0700

    Push #6 (opdracht compleet)

--- src/blackjack.java
+++ src/blackjack.java
@@ -27,18 +27,45 @@ public class blackjack {
 		int totaal = 0;
 		int game_counter = 0;
 		
+		//Kaart 1 uitdelen
+		System.out.println("- Kaart 1: " + kaarten[game_counter].naam + ".");
+		totaal += kaarten[game_counter].waarde;
+		game_counter++;
+		
+		// Kaart 2 uitdelen (met dubbele-aas check)
+		System.out.println("- Kaart 2: " + kaarten[game_counter].naam + ".");
+		if (totaal == 11 && kaarten[game_counter].plaatje.equals("aas")) {
+			totaal ++;
+		} else {
+			totaal += kaarten[game_counter].waarde;
+		}		
+		game_counter++;
+		System.out.println("[Totaal: " + totaal + "]");
+		
 		while (totaal < 21) {
 			//	Speler input
 			System.out.println("--- Kaarten over: " + (kaarten.length - game_counter) + " ---    (Nieuwe kaart: k | Pas: p | Stop spel: q)");
 			Scanner inputObj = new Scanner(System.in);
 			String input = inputObj.nextLine();
+			inputObj.close();
 			
 			//Controleren input
 			if (input.equals("k")==true) {				
 				//	Kaart tonen en bij totaal optellen
 				System.out.println("Je kaart is de: " + kaarten[game_counter].naam + ".");
-				totaal += kaarten[game_counter].waarde;
-				System.out.println("Totaal: " + totaal);
+				
+				// Aas check (1 of 11)
+				if (kaarten[game_counter].plaatje.equals("aas")==true) {
+					if (totaal > 10) {
+						totaal += 1;
+					} else {
+						totaal += kaarten[game_counter].waarde;
+					}
+				} else {
+					totaal += kaarten[game_counter].waarde;
+				}
+				
+				System.out.println("[Totaal: " + totaal + "]");
 				game_counter++;
 				
 			//	Passen
@@ -59,7 +86,7 @@ public class blackjack {
 		}
 		// Uitslag tonen
 		if (totaal == 21) {
-			System.out.println("--- BLACKJACK! ---");
+			System.out.println("---< BLACKJACK! >---");
 		} else if (totaal > 21) {
 			System.out.println("Je hebt verloren!");
 		} else if (totaal < 21) {

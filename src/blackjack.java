d0663400ff032df34dc47e5900eb622c58480992
mghgroenen <mgh.groenen@gmail.com>
2020-09-01 04:24:48 -0700

    Push #7 (nu met inzet)

--- src/blackjack.java
+++ src/blackjack.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class blackjack {
 	public static void main(String[] args) {
@@ -19,35 +20,56 @@ public class blackjack {
 		}
 		Collections.shuffle(Arrays.asList(kaarten));
 		
+		//	Print geschudde kaarten
 //		for(Kaart check : kaarten) {
 //		System.out.println(check.naam);
 //		}	
 		
 		//	Blackjack-loop
 		int totaal = 0;
+		int bank = 0;
 		int game_counter = 0;
 		
-		//Kaart 1 uitdelen
+		//Inzetten
+		System.out.println("Welkom aan de tafel. Hoeveel euro zet je in?");
+		wacht();
+		Scanner inzetObj = new Scanner(System.in);
+		int inzet = inzetObj.nextInt();
+		wacht();
+				
 		System.out.println("- Kaart 1: " + kaarten[game_counter].naam + ".");
+		kaarten[game_counter].aan_wie = "speler";
 		totaal += kaarten[game_counter].waarde;
 		game_counter++;
 		
 		// Kaart 2 uitdelen (met dubbele-aas check)
+		wacht();
 		System.out.println("- Kaart 2: " + kaarten[game_counter].naam + ".");
 		if (totaal == 11 && kaarten[game_counter].plaatje.equals("aas")) {
 			totaal ++;
 		} else {
 			totaal += kaarten[game_counter].waarde;
 		}		
+		kaarten[game_counter].aan_wie = "speler";
 		game_counter++;
-		System.out.println("[Totaal: " + totaal + "]");
+		wacht();
 		
+		// Kaart bank uitdelen
+		System.out.println(" ");
+		System.out.println("- Bank: " + kaarten[game_counter].naam + ".");
+		bank += kaarten[game_counter].waarde;
+		kaarten[game_counter].aan_wie = "bank";
+		game_counter++;
+		wacht();
+		System.out.println("[Speler: " + totaal + "][Bank: " + bank + "]");
+		
+		// Speler loop
 		while (totaal < 21) {
 			//	Speler input
-			System.out.println("--- Kaarten over: " + (kaarten.length - game_counter) + " ---    (Nieuwe kaart: k | Pas: p | Stop spel: q)");
+			wacht();
+			System.out.println("====== (Nieuwe kaart: k | Pas: p | Stop spel: q) ======");
 			Scanner inputObj = new Scanner(System.in);
 			String input = inputObj.nextLine();
-			inputObj.close();
 			
 			//Controleren input
 			if (input.equals("k")==true) {				
@@ -65,12 +87,14 @@ public class blackjack {
 					totaal += kaarten[game_counter].waarde;
 				}
 				
-				System.out.println("[Totaal: " + totaal + "]");
+				System.out.println("[Speler: " + totaal + "][Bank: " + bank + "]");
+				kaarten[game_counter].aan_wie = "speler";
 				game_counter++;
 				
 			//	Passen
 			} else if (input.equals("p")==true) {
-				System.out.println("Je hebt gepast. Totaal: " + totaal);
+				System.out.println("Je hebt gepast.");
+				System.out.println("[Speler: " + totaal + "][Bank: " + bank + "]");
 				break;
 			
 			// Afsluiten
@@ -81,22 +105,62 @@ public class blackjack {
 			//Foutieve input
 			} else {
 				System.out.println("Input is niet k, p, of q.");
-				System.out.println("Totaal: " + totaal);
+				System.out.println("[Speler: " + totaal + "][Bank: " + bank + "]");
 			}
 		}
-		// Uitslag tonen
-		if (totaal == 21) {
-			System.out.println("---< BLACKJACK! >---");
-		} else if (totaal > 21) {
-			System.out.println("Je hebt verloren!");
-		} else if (totaal < 21) {
-			;
-			}
 		
-		//	Getrokken kaarten tonen
-		System.out.println("Getrokken kaarten: ");
+		// Bank loop
+		wacht();
+		if (totaal < 22) {
+			System.out.println("-----------------------------------------------------------------------");
+			System.out.println("De bank is nu aan de beurt.");
+			while (bank < 17) {
+				wacht();
+				wacht();
+				wacht();
+				System.out.println("De bank trekt een: " + kaarten[game_counter].naam + ".");
+				if (kaarten[game_counter].plaatje.equals("aas")==true) {
+					if (bank > 6) {
+						totaal += 1;
+					} else {
+						bank += kaarten[game_counter].waarde;
+					}
+				} else {
+					bank += kaarten[game_counter].waarde;
+				}
+				kaarten[game_counter].aan_wie = "bank";
+				game_counter++;
+				wacht();
+				System.out.println("[Speler: " + totaal + "][Bank: " + bank + "]");
+			}
+		}
+		
+		// Uitslag speler
+		wacht();
+		if (totaal == 21 && bank != 21) {
+			System.out.println("---< BLACKJACK! >---");
+			wacht();
+			System.out.println("Je wint: " + (1.5*inzet) + " euro.");
+		} else if (totaal > 21) {
+			System.out.println("Helaas. Je hebt " + inzet + " euro verloren!");
+		} else if (totaal > bank) {
+			System.out.println("Gefeliciteerd! Je wint: " + inzet + " euro.");
+		} else if (totaal < bank && bank < 22) {
+			System.out.println("Helaas. Je hebt " + inzet + " euro verloren!");
+		} else if (totaal < bank && bank > 21) {
+			System.out.println("Gefeliciteerd! Je wint: " + inzet + " euro.");	
+		} else if (totaal == bank) {
+			System.out.println("Gelijkspel. Je krijgt je inzet terug.");
+		}
+		
+		//	Gespeelde kaarten tonen
+		wacht();
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println("Gespeelde kaarten: ");
+		wacht();
 		for(int i=0; i<game_counter;i++) {
-			System.out.println("- " + kaarten[i].naam);
+			wacht();
+			System.out.println("- " + kaarten[i].naam + "		(" + kaarten[i].aan_wie + ")");
 		}
 		System.exit(0);
 	}
@@ -104,6 +168,7 @@ public class blackjack {
 		String naam;
 		String kleur;
 		String plaatje;
+		String aan_wie;
 		int waarde;
 		
 		Kaart(String kleur, String plaatje) {
@@ -137,6 +202,13 @@ public class blackjack {
 			} else if (plaatje.equals("aas")==true) {
 				waarde = 11;
 			}
+		}
+	}
+	public static void wacht() {
+		try {
+		    Thread.sleep(1000);                 
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
 		}
 	}
 }
